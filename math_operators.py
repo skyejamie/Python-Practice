5b6862449591edae468264c55f6a957d3257355d
hazel <186485609+skyejamie@users.noreply.github.com>
2025-05-16 21:10:34 -0400

    python files

---
+++ math_operators.py
@@ -0,0 +1,36 @@
+# mathematical operators
+expression = 2 + 2 # expression variable example
+print (expression) # prints the result (must have a space between "print" and "(var name)", not sure why but whatever)
+
+# variables in the expression
+
+#division variable expression
+a = 144 # you can put in any letter, doesn't matter
+b = 12
+print(a / b) # (unlike the one above, you don't need to use space) this divides both variables and prints the result, you can use any of the following operators ( + (add), - (subtract & negative numbers), * (multiply), / (divide), ** (exponents), % (remainders) )
+
+# exponent variable expression
+f = 5
+g = 10
+print(f ** g) # calculates the exponent variables and prints the result
+
+# remainder variable expression
+y = 23
+z = 2
+print(y % z) # prints the left over
+
+# negative variable expression
+l = 9
+m = 5
+a = 4
+o = 2
+print(-l + m + a + o) # (the 9 in the "L" variable turns into a -9 because the "-" sign is infront of the "L" variable in the print function) adds them all and prints the result
+
+# cubed math operator
+cubed = 5 ** 3 # add ** and then (number) turns the second number into an exponent
+print (cubed) # prints the result of the cube
+
+# quartic variable expression
+c = 2
+d = 4
+print(c ** d) # prints the result turning the number in the "c" variable raise to the power of whatever number that's in the "d" variable (which in this case, it's 4)
---
+++ variables.py
@@ -0,0 +1,7 @@
+# variables
+programmingLanguage = "Python" # variable
+print(programmingLanguage) # print the variable
+
+sentence = "I like to " # variable 1
+word = "breathe" # variable 2
+print(sentence + word) # prints both variables
